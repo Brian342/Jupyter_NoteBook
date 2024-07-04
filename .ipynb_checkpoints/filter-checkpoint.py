c5d6cd003539af9ceb840e662f6c6a57186f290a
Brian342 <migelbrian3@gmail.com>
2024-07-04 20:53:08 +0300

    Jupyter NoteBook

--- .ipynb_checkpoints/filter-checkpoint.py
+++ .ipynb_checkpoints/filter-checkpoint.py
@@ -13,7 +13,7 @@ image = Image.open(sys.arvg[1]).convert("RGB")
 
 # Filter mage according to edge detection kernel
 filtered = image.filter(ImageFilter.Kernel(
-    size =(3, 3),
+    size =(3,3),
     kernel=[-1,-1,-1,-1,8,-1,-1,-1,-1],
     scale=1
 ))
